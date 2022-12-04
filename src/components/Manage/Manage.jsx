499313f4d77b470bb654133ba06f2034893a273e
Pavlo Pysaniuk <93548330+pasha2708@users.noreply.github.com>
2022-12-04 09:13:55 +0200

    fix loading

--- src/components/Manage/Manage.jsx
+++ src/components/Manage/Manage.jsx
@@ -8,101 +8,95 @@ import { Container } from '../../App';
 import { StyledInput, StyledQuestion } from './Manage.styles';
 
 const Manage = () => {
-	const [logged, setLogged] = React.useState(false);
-	const [inputValue, setInputValue] = React.useState('');
-	const [token, setToken] = React.useState('');
-	const [questions, setQuestions] = React.useState([]);
-	const [loading, setLoading] = React.useState(true);
+  const [logged, setLogged] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState('');
+  const [token, setToken] = React.useState('');
+  const [questions, setQuestions] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-	const fetchData = () => {
-		axios
-			.get('https://asq-a-question-be.vercel.app/questions', {
-				headers: { Authorization: token },
-			})
-			.then(({ data }) => {
-				setQuestions(data);
-				setLoading(false);
-			})
-			.catch((e) => console.error(e));
-	};
+  const fetchData = () => {
+    token &&
+      axios
+        .get('https://asq-a-question-be.vercel.app/questions', {
+          headers: { Authorization: token },
+        })
+        .then(({ data }) => {
+          setQuestions(data);
+          setLoading(false);
+        })
+        .catch((e) => console.error(e));
+  };
 
-	React.useEffect(() => {
-		if (!token) {
-			const tokenCookie = document.cookie
-			.split('; ')
-			.find((row) => row.startsWith('token='))
-			?.split('=')[1];
-		if (tokenCookie) {
-			setLogged(true);
-			setToken(tokenCookie);
-			fetchData()
-		}
-		}
-		
-	}, [logged, fetchData]);
+  React.useEffect(() => {
+      const tokenCookie = document.cookie
+        .split('; ')
+        .find((row) => row.startsWith('token='))
+        ?.split('=')[1];
+      if (tokenCookie) {
+        setLogged(true);
+        setToken(tokenCookie);
+        !questions.length && fetchData();
+      }
+  }, [fetchData]);
 
-	const getToken = () => {
-		axios
-			.post('https://asq-a-question-be.vercel.app/auth', {
-				password: md5(inputValue),
-			})
-			.then(() => {
-				setLogged(true);
-				setToken(md5(inputValue));
-				document.cookie = `token=${md5(inputValue)}`;
-				fetchData();
-			})
-			.catch(() => alert('Невірний пароль'));
-	};
+  const getToken = () => {
+    axios
+      .post('https://asq-a-question-be.vercel.app/auth', {
+        password: md5(inputValue),
+      })
+      .then(() => {
+        setLogged(true);
+        setToken(md5(inputValue));
+        document.cookie = `token=${md5(inputValue)}`;
+        !questions.length && fetchData();
+      })
+      .catch(() => alert('Невірний пароль'));
+  };
 
-	const deleteQuestion = (id) => {
-		if (window.confirm('Видалити питання?')) {
-			axios
-				.delete(`https://asq-a-question-be.vercel.app/questions/${id}`, {
-					headers: { Authorization: token },
-				})
-				.then(({ data }) => setQuestions(data));
-		}
-	};
-	return (
-		<Container>
-			{!logged && (
-				<>
-					<StyledInput
-						value={inputValue}
-						onChange={(e) => setInputValue(e.target.value)}
-					/>
-					<Button
-						style={{ backgroundColor: '#e16f3b' }}
-						variant='contained'
-						onClick={() => getToken()}
-					>
-						Вхід
-					</Button>
-				</>
-			)}
-			{questions.length === 0 && logged && !loading && (
-				<h1>Питань немає</h1>
-			)}
-			{logged && loading && <h1>Завантажується...</h1>}
-			{logged &&
-				questions?.map((item) => (
-					<StyledQuestion>
-						<span>
-							Дата: {moment(item.date).format('DD.MM.YYYY HH:mm')}
-						</span>
-						<span>{item.text}</span>
-						<Button
-							style={{ backgroundColor: '#e16f3b' }}
-							variant='contained'
-							onClick={() => deleteQuestion(item._id)}
-						>
-							Видалити
-						</Button>
-					</StyledQuestion>
-				))}
-		</Container>
-	);
+  const deleteQuestion = (id) => {
+    if (window.confirm('Видалити питання?')) {
+      axios
+        .delete(`https://asq-a-question-be.vercel.app/questions/${id}`, {
+          headers: { Authorization: token },
+        })
+        .then(({ data }) => setQuestions(data));
+    }
+  };
+  return (
+    <Container>
+      {!logged && (
+        <>
+          <StyledInput
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            style={{ backgroundColor: '#e16f3b' }}
+            variant='contained'
+            onClick={() => getToken()}
+          >
+            Вхід
+          </Button>
+        </>
+      )}
+      {questions.length === 0 && logged && !loading && <h1>Питань немає</h1>}
+      {logged && loading && <h1>Завантажується...</h1>}
+      {logged &&
+        questions?.map((item) => (
+          <StyledQuestion>
+            <span>Дата: {moment(item.date).format('DD.MM.YYYY HH:mm')}</span>
+            <span>{item.text}</span>
+            <Button
+              style={{ backgroundColor: '#e16f3b' }}
+              variant='contained'
+              onClick={() => deleteQuestion(item._id)}
+            >
+              Видалити
+            </Button>
+          </StyledQuestion>
+        ))}
+    </Container>
+  );
 };
 
 export default Manage;
